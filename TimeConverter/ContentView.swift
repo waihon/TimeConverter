c53b0fdaeefe473cc735bcfbd18bd4fb5e5a67bd
Waihon Yew <yewwaihon@gmail.com>
2022-04-24 09:27:00 +0800

    Create initial version of UI

--- TimeConverter/ContentView.swift
+++ TimeConverter/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    static let units = ["Seconds", "Minutes", "Hours", "Days"]
+    @State private var inputUnit = units[0]
+    @State private var outputUnit = units[0]
+    @State private var convertFrom = 1.0
+
+    var convertedTo: Double {
+        convertFrom
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                Picker("Input unit", selection: $inputUnit) {
+                    ForEach(Self.units, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(.segmented)
+            } header: {
+                Text("Input Unit")
+            }
+
+            Section {
+                Picker("Output unit", selection: $outputUnit) {
+                    ForEach(Self.units, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                .pickerStyle(.segmented)
+            } header: {
+                Text("Output Unit")
+            }
+
+            Section {
+                TextField("Convert From", value: $convertFrom, format: .number)
+                    .keyboardType(.decimalPad)
+            } header: {
+                Text("Convert From")
+            }
+
+            Section {
+                Text("\(convertedTo.formatted())")
+            } header: {
+                Text("Converted To")
+            }
+        }
     }
 }
 
